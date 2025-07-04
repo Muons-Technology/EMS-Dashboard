e711e8e671db48f0ff8fb3c80ff4613023e4d6c5
NtokozoMahlaela1 <Ntokozo.Mahlaela@muons.technolgy>
2025-07-04 20:52:34 +0200

    Changed styling

--- ems-dashboard/src/Utils/mockData.js
+++ ems-dashboard/src/Utils/mockData.js
@@ -2,4 +2,5 @@ export const patients = [
   { id: 1, name: "Tshidi Mkhwanazi", admittedAt: "2025-07-01", labPending: true },
   { id: 2, name: "Musa Ndima", admittedAt: "2025-07-01", labPending: false },
   { id: 3, name: "Ntokozo Mokoena", admittedAt: "2025-06-30", labPending: true },
+  { id: 3, name: "Lerato Mokoena", admittedAt: "2025-06-30", labPending: true },
 ];
